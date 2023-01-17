5f7ba5e30003d13699ea3384c867c25fefbb5f2c
20jmorrison <92942539+20jmorrison@users.noreply.github.com>
2023-01-17 14:59:04 -0500

    First Upload

---
+++ App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <SignIn></SignIn>
+  </div>
+</template>
+
+<script>
+import SignIn from "./components/SignIn.vue";
+export default {
+  name: "app",
+  components: {
+    SignIn,
+  },
+};
+</script>
+
+<style>
+body {
+  background-color: #f0f0f0;
+}
+
+#app {
+  width: 1024px;
+  margin: auto;
+}
+</style>
