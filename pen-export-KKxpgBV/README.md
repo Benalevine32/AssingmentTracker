656230b35e3d1719657aa42df061e77139467583
20jmorrison <92942539+20jmorrison@users.noreply.github.com>
2023-02-16 21:04:55 -0500

    Add files via upload

---
+++ pen-export-KKxpgBV/README.md
@@ -0,0 +1,4 @@
+# 
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/20jmorrison/pen/KKxpgBV](https://codepen.io/20jmorrison/pen/KKxpgBV).
+
---
+++ pen-export-KKxpgBV/dist/script.js
@@ -0,0 +1,18 @@
+document.getElementsById("timePGB")[0].style.fontSize = "6vw";
+
+var bar = new ProgressBar.Line(timePGB, {
+  strokeWidth: 4,
+  easing: 'easeInOut',
+  duration: 1400,
+  color: '#FFEA82',
+  trailColor: '#eee',
+  trailWidth: 1,
+  svgStyle: { width: '100%', height: '100%' },
+  from: { color: '#FFEA82' },
+  to: { color: '#ED6A5A' },
+  step: (state, bar) => {
+    bar.path.setAttribute('stroke', state.color);
+  } });
+
+
+bar.animate(1.0); // Number from 0.0 to 1.0
