b1ec544600255a2b06c82d6288383643e583472c
lamywmpaul@gmail.com <lamywmpaul@gmail.com>
2023-04-02 16:38:28 -0400

    Made changes to AWS_RDS.js
    added more requests for/with app

--- NodeServer/AWS_RDS.js
+++ NodeServer/AWS_RDS.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const app = express();
 
 app.use(cors());
+app.use(express.json());
 
 const connection = mysql.createConnection({
     host: 'assignmenttracker.ci3higxjoes6.us-east-1.rds.amazonaws.com',
@@ -22,6 +23,30 @@ app.get('/api/classes', (req, res) => {
       res.json(results);
     });
   });
+
+  // GET -> get the class with the specified class_ID
+  app.get('/api/classes/:class_id', (req, res) => {
+    const class_id = req.params.class_id;
+    connection.query('SELECT * FROM classes WHERE class_id = ?', [class_id], (error, results) => {
+      if (error) {
+        console.error('Error executing query Classes:', error);
+        return res.status(500).json({ error: 'Database error' });
+      }
+      res.json(results);
+    });
+  });
+
+app.get('/api/assignments', (req, res) => {
+    console.log("Made it past get request")
+    connection.query('SELECT * FROM assignments', (error, results) => {
+      if (error) {
+        console.log("output that there was an error")
+        console.error('Error executing query Assignments:', error);
+        return res.status(500).json({ error: 'Database error' });
+      }
+      res.json(results);
+    });
+  });
   
   app.get('/api/top3Assignments', (req, res) => {
     connection.query('SELECT * FROM assignments ORDER BY difficulty DESC LIMIT 3', (error, results) => {
@@ -32,6 +57,23 @@ app.get('/api/classes', (req, res) => {
       res.json(results);
     });
   });
+
+  // POST -> ADD an assignment into assignments table.
+  app.post('/api/assignments', (req, res) => {
+    const { assignment_id, class_id, description, difficulty, dueDate, user_id } = req.body;
+    if (!assignment_id || !class_id || !description || !difficulty || !dueDate || !user_id) {
+      return res.status(400).json({ error: 'Invalid request parameters' });
+    }
+    connection.query('INSERT INTO assignments (assignment_id, class_id, description, difficulty, dueDate, user_id) VALUES (?, ?, ?, ?, ?, ?)', [assignment_id, class_id, description, difficulty, dueDate, user_id], (error, results) => {
+      if (error) {
+        console.error('Error executing query:', error);
+        return res.status(500).json({ error: 'Database error' });
+      }
+      res.json(results);
+    });
+  });
+
+
 const port = process.env.PORT || 3001;
 
 app.listen(port, ()=>{
