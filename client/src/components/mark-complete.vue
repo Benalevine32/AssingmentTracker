197ef913851cea4b562d8767d220d4fe5772fe9c
20jmorrison <92942539+20jmorrison@users.noreply.github.com>
2023-04-17 17:08:20 -0400

    added mark as complete button

---
+++ client/src/components/mark-complete.vue
@@ -0,0 +1,27 @@
+<template>
+  <button id="complete" @click="celebrate()">Done ✓</button>
+</template>
+
+<script>
+import VueConfetti from "vue-confetti";
+import Vue from "vue";
+import axios from "axios";
+Vue.use(VueConfetti);
+
+export default {
+  name: "mark-complete",
+  props: {
+    assId: Number
+  },
+  methods: {
+    async celebrate() {
+      var id = this.assId;
+      axios.get("http://localhost:3001/api/markComplete/" + id);
+      this.$confetti.start();
+      await new Promise((resolve) => setTimeout(resolve, 1500));
+      this.$confetti.stop();
+      window.location.reload();
+    },
+  },
+};
+</script>
